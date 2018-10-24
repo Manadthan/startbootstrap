8967b004dec851f53137818e56068297f7c78e3d
Adrien Raffy <adrienraffy@macbook-pro-de-adrien.home>
2018-10-24 21:25:47 +0200

    Working contact form

--- contact.php
+++ contact.php
@@ -4,7 +4,7 @@
  */
 
 // an email address that will be in the From field of the email.
-$from = 'contact@manadthan.ovh>';
+$from = 'contact@manadthan.ovh';
 
 // an email address that will receive the email with the output of the form
 $sendTo = 'raffy.adrien@gmail.com';
