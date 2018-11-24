b2d45390870a295fc6784e1a6b4e594784b53985
Stephanie Cano <stephanie.cano@diag-dechets.fr>
2018-11-24 14:44:55 +0100

    Publish website

--- js/map.js
+++ js/map.js
@@ -1,5 +1,5 @@
 $(function () {
-    var mymap = L.map('mapid').setView([43.575, 1.48], 11);
+    var mymap = L.map('mapid').setView([43.56, 1.48], 11);
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
