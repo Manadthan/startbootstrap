d9956252613876d467c0d283b0f494b91cb88234
Adrien Raffy <raffy.adrien@gmail.com>
2020-11-01 15:53:10 +0100

    Update map

--- js/map.js
+++ js/map.js
@@ -4,7 +4,7 @@ $(function () {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
         id: 'mapbox.streets',
-        accessToken: 'pk.eyJ1IjoibWFuYWR0aGFuIiwiYSI6ImNqbXQ3bmpqbDJkaWkzcWs0MnFha2NqMzQifQ.RlyHxBFh71phG1x9Ce94fg'
+        accessToken: 'pk.eyJ1IjoibWFuYWR0aGFuIiwiYSI6ImNrZ3o3MmozajBsZTcyeW52dXQxMmc5ajIifQ.V-QbOqrIPNCtdztUBKEoAA'
     }).addTo(mymap);
     var marker = L.marker([43.499324, 1.578366]).addTo(mymap).bindPopup('Diag-Déchets<br>Belberaud').openPopup();
 });
